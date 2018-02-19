b796d4b7e9050e121b9c74ee77131b5d7331f343
Verohub <vero_nik_28@hotmail.com>
2018-02-18 22:27:17 -0600

    Se agrega boton para descarga de imagen

--- app/src/main/java/mx/unam/aragon/fes/diplo/gitandroid/MainActivity.java
+++ app/src/main/java/mx/unam/aragon/fes/diplo/gitandroid/MainActivity.java
@@ -1,14 +1,23 @@
 package mx.unam.aragon.fes.diplo.gitandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
-private Button miBoton,miSegBoton;
+private Button miBoton,miSegBoton,miTerBoton;
+private Bitmap imgdescarga;
+private ImageView miimagen;
 private TextView miVista;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +27,9 @@ private TextView miVista;
         miBoton=findViewById(R.id.button);
         miSegBoton=findViewById(R.id.button2);
         miVista = findViewById(R.id.textView);
+        miTerBoton = findViewById(R.id.button3);
+        miimagen=findViewById(R.id.imageView2);
+
 
         miBoton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +45,48 @@ private TextView miVista;
             }
         });
 
+        miTerBoton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://vignette.wikia.nocookie.net/toontown/images/8/82/Toony_Toons.png/revision/latest?cb=20110313052410";
+                try{
+                    new Descargarimag().execute(new URL(url));
+                }
+                catch (Exception e){
+                    android.util.Log.e("diplo tag","error al descargar imagen");
+
+                }
+            }
+        });
+
 
     }
+
+    class Descargarimag extends AsyncTask<URL, Integer,Bitmap> {
+        @Override
+        protected Bitmap doInBackground(URL... urls) {
+            try{
+                Log.i("diplo tag","iniciando descarga...");
+                imgdescarga= BitmapFactory.decodeStream(urls[0].openConnection().getInputStream());
+            }
+            catch (Exception e){
+                Log.e ("diplo","Error:" + e.toString());
+            }
+            return  imgdescarga;
+        }
+    @Override
+    protected  void  onProgressUpdate(Integer... values){
+            super.onProgressUpdate(values[0]);
+            Log.i("diplo tag" , ""+ values[0]);
+}
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            //establecer el imageView sobre el layout
+            miimagen.setImageBitmap(imgdescarga);
+            super.onPostExecute(bitmap);
+        }
+    }
+
+
 }
